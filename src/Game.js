aa0c26ef5cadf92c226dfe5f9abcea96384589cb
didehdgns <67950517+didehdgns@users.noreply.github.com>
2020-10-05 16:30:48 +0900

    201005

--- src/Game.js
+++ src/Game.js
@@ -4,21 +4,32 @@ import "./Game.css";
 
 function Game({ year, title, summary, poster, genres }) {
   return (
-    <div className="game">
-      <img src={poster} alt={title} title={title} />
-      <div className="game_data">
-        <h3 className="game__title" style={{}}>
-          {title}
-        </h3>
-        <h5 className="game__year">{year}</h5>
-        <ul className="game__genres">
-          {genres.map((genre, index) => (
-            <li key={index} className="genres__genre">
-              {genre}
-            </li>
-          ))}
-        </ul>
-        <p className="game__summary">{summary.slice(0, 180)}...</p>
+    <div>
+      <div>
+        <div>
+          <ul>
+            <li>전략</li>
+            <li>시뮬레이션</li>
+            <li>롤플레잉</li>
+          </ul>
+        </div>
+      </div>
+      <div className="game">
+        <img src={poster} alt={title} title={title} />
+        <div className="game_data">
+          <h3 className="game__title" style={{}}>
+            {title}
+          </h3>
+          <h5 className="game__year">{year}</h5>
+          <ul className="game__genres">
+            {genres.map((genre, index) => (
+              <li key={index} className="genres__genre">
+                {genre}
+              </li>
+            ))}
+          </ul>
+          <p className="game__summary">{summary.slice(0, 180)}...</p>
+        </div>
       </div>
     </div>
   );
