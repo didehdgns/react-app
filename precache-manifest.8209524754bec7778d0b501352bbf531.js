2c7c1fa340182e41d1a59cf2d872b39980434ed4
didehdgns <67950517+didehdgns@users.noreply.github.com>
2020-10-13 21:27:26 +0900

    Updates

---
+++ precache-manifest.8209524754bec7778d0b501352bbf531.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "84d184f9a891888c17080d769dac442f",
+    "url": "/react-app/index.html"
+  },
+  {
+    "revision": "0e3c7078c6d4d66c7d84",
+    "url": "/react-app/static/css/main.abe2816c.chunk.css"
+  },
+  {
+    "revision": "90d90b713e00d5940ff2",
+    "url": "/react-app/static/js/2.95a621b4.chunk.js"
+  },
+  {
+    "revision": "f9e7e68e90600aabdd7d1f759413461d",
+    "url": "/react-app/static/js/2.95a621b4.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "0e3c7078c6d4d66c7d84",
+    "url": "/react-app/static/js/main.2786a618.chunk.js"
+  },
+  {
+    "revision": "e2c6b0a16d423a796dce",
+    "url": "/react-app/static/js/runtime-main.9a102076.js"
+  }
+]);
