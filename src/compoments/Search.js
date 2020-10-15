834f215c2699f31486aa67e7e38f13928e064a9c
didehdgns <67950517+didehdgns@users.noreply.github.com>
2020-10-15 23:09:33 +0900

    2020.10.15 ver2

--- src/compoments/Search.js
+++ src/compoments/Search.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import { VscSearch } from "react-icons/vsc";
-import './Search.css'
+import './Search.css';
+import axios from 'axios'
 
 class Search extends React.Component{
     constructor(props){
@@ -11,10 +12,41 @@ class Search extends React.Component{
             loading: false,
             message: ''
         }
+
+        this.cancel = '';
+    }
+
+    fetchSearchResults = (updatedPageNo = '', query) => {
+        const pageNumber = updatedPageNo ? `&page=${updatedPageNo}` : '';
+        const searchUrl = `https://game-app-9c71b.firebaseio.com/data/games/${query}.json`
+
+        if( this.cancel){
+            this.cancel.cancel();
+        }
+        this.cancel = axios.CancelToken.source();
+
+        axios.get(searchUrl, {
+            cancelToken: this.cancel.token
+        })
+        .then(res => {
+            // const resultNotfoundMsg = 
+            console.warn(res.data);
+        })
+        .catch(error => {
+            if(axios.isCancel(error) || error){
+                this.setState({
+                    loading:false,
+                    message: 'Faled to fetch the data, Please check network'
+                })
+            }
+        })
     }
     handleOnInputChange = (event) => {
         const query = event.target.value;
-        this.setState( state:{query:query, loading: true, message: ''});
+        this.setState( {query:query, loading: true, message: ''},() => {
+            this.fetchSearchResults(1, query);
+        });
+        
       };
     render(){
         const {query} = this.state;
